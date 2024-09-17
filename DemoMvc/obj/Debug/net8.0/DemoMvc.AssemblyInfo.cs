b2fba711a022731a13d855138da63c110f99fea1
Phuc <phucpd17702@gmail.com>
2024-09-17 15:37:20 +0700

    Update CRUD

--- DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+514b6b1b6928e488808c9d24b5ee473e8e0fda3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc0ea37c0f2f1ad7b6d5cd643936b26c3339a9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
